7d1255b686989cc8ad2b93b73bc92cf7faa93cca
smdn <smdn@smdn.jp>
2020-04-08 21:20:04 +0900

    make ISetExtensions internal

--- Smdn.Reflection.ReverseGenerating/src/Smdn.Reflection.ReverseGenerating/ISetExtensions.cs
+++ Smdn.Reflection.ReverseGenerating/src/Smdn.Reflection.ReverseGenerating/ISetExtensions.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 
-public static class ISetExtensions {
-  public static void AddRange<T>(this ISet<T> s, IEnumerable<T> sequence)
-  {
-    foreach (var e in sequence)
-      s.Add(e);
+namespace Smdn.Reflection.ReverseGenerating {
+  internal static class ISetExtensions {
+    public static void AddRange<T>(this ISet<T> s, IEnumerable<T> sequence)
+    {
+      foreach (var e in sequence)
+        s.Add(e);
+    }
   }
 }
+
