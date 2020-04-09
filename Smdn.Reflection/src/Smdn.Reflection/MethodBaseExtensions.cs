e44e77f775d31dddc51c0eaaccd7fa41580371ac
smdn <smdn@smdn.jp>
2020-04-10 03:22:57 +0900

    add MethodBaseExtensions.GetNameType()

--- Smdn.Reflection/src/Smdn.Reflection/MethodBaseExtensions.cs
+++ Smdn.Reflection/src/Smdn.Reflection/MethodBaseExtensions.cs
@@ -57,5 +57,55 @@ namespace Smdn.Reflection {
 
       return null;
     }
+
+    private static readonly Dictionary<string, MethodSpecialName> specialMethodNames = new Dictionary<string, MethodSpecialName>(StringComparer.Ordinal) {
+      // comparison
+      {"op_Equality", MethodSpecialName.Equality},
+      {"op_Inequality", MethodSpecialName.Inequality},
+      {"op_LessThan", MethodSpecialName.LessThan},
+      {"op_GreaterThan", MethodSpecialName.GreaterThan},
+      {"op_LessThanOrEqual", MethodSpecialName.LessThanOrEqual},
+      {"op_GreaterThanOrEqual", MethodSpecialName.GreaterThanOrEqual},
+
+      // unary
+      {"op_UnaryPlus", MethodSpecialName.UnaryPlus},
+      {"op_UnaryNegation", MethodSpecialName.UnaryNegation},
+      {"op_LogicalNot", MethodSpecialName.LogicalNot},
+      {"op_OnesComplement", MethodSpecialName.OnesComplement},
+      {"op_True", MethodSpecialName.True},
+      {"op_False", MethodSpecialName.False},
+      {"op_Increment", MethodSpecialName.Increment},
+      {"op_Decrement", MethodSpecialName.Decrement},
+
+      // binary
+      {"op_Addition", MethodSpecialName.Addition},
+      {"op_Subtraction", MethodSpecialName.Subtraction},
+      {"op_Multiply", MethodSpecialName.Multiply},
+      {"op_Division", MethodSpecialName.Division},
+      {"op_Modulus", MethodSpecialName.Modulus},
+      {"op_BitwiseAnd", MethodSpecialName.BitwiseAnd},
+      {"op_BitwiseOr", MethodSpecialName.BitwiseOr},
+      {"op_ExclusiveOr", MethodSpecialName.ExclusiveOr},
+      {"op_RightShift", MethodSpecialName.RightShift},
+      {"op_LeftShift", MethodSpecialName.LeftShift},
+
+      // type cast
+      {"op_Explicit", MethodSpecialName.Explicit},
+      {"op_Implicit", MethodSpecialName.Implicit},
+    };
+
+    public static MethodSpecialName GetNameType(this MethodBase m)
+    {
+      if (!m.IsSpecialName)
+        return MethodSpecialName.None;
+
+      if (specialMethodNames.TryGetValue(m.Name, out var methodSpecialName))
+        return methodSpecialName;
+
+      if (m is ConstructorInfo)
+        return MethodSpecialName.Constructor;
+
+      return MethodSpecialName.Unknown;
+    }
   }
 }
--- Smdn.Reflection/test/Smdn.Reflection/MethodBaseExtensions.cs
+++ Smdn.Reflection/test/Smdn.Reflection/MethodBaseExtensions.cs
@@ -66,6 +66,93 @@ namespace Smdn.Reflection {
 
       Assert.AreEqual(expectedMethodName, explicitInterfaceMethod?.Name, methodName);
     }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor)]
+    class ExpectedMethodSpecialNameAttribute : Attribute {
+      public MethodSpecialName Expected { get; }
+
+      public ExpectedMethodSpecialNameAttribute(MethodSpecialName expected)
+      {
+        this.Expected = expected;
+      }
+    }
+
+    class SpecialMethods {
+      public class V { }
+      public class W { }
+
+      public class C : IDisposable {
+        [ExpectedMethodSpecialName(MethodSpecialName.Constructor)] public C() { }
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] ~C() { }
+
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] public void Deconstruct(out int x, out int y, out int z) => throw new NotImplementedException();
+
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] void M() => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] void IDisposable.Dispose() => throw new NotImplementedException();
+
+        // unary operators
+        [ExpectedMethodSpecialName(MethodSpecialName.UnaryPlus)] public static C operator +(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.UnaryNegation)] public static C operator -(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.LogicalNot)] public static C operator !(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.OnesComplement)] public static C operator ~(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.True)] public static bool operator true(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.False)] public static bool operator false(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Increment)] public static C operator ++(C c) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Decrement)] public static C operator --(C c) => throw new NotImplementedException();
+
+        // binary operators
+        [ExpectedMethodSpecialName(MethodSpecialName.Addition)] public static C operator +(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Subtraction)] public static C operator -(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Multiply)] public static C operator *(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Division)] public static C operator /(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Modulus)] public static C operator %(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.BitwiseAnd)] public static C operator &(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.BitwiseOr)] public static C operator |(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.ExclusiveOr)] public static C operator ^(C x, C y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.RightShift)] public static C operator >>(C x, int y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.LeftShift)] public static C operator <<(C x, int y) => throw new NotImplementedException();
+
+        // type cast
+        [ExpectedMethodSpecialName(MethodSpecialName.Explicit)] public static explicit operator C(V v) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Explicit)] public static explicit operator V(C c) => throw new NotImplementedException();
+
+        [ExpectedMethodSpecialName(MethodSpecialName.Implicit)] public static implicit operator C(W w) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Implicit)] public static implicit operator W(C c) => throw new NotImplementedException();
+      }
+
+      public class P : IEquatable<P>, IComparable<P> {
+        // comparison
+        [ExpectedMethodSpecialName(MethodSpecialName.Equality)] public static bool operator ==(P x, P y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.Inequality)] public static bool operator !=(P x, P y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.LessThan)] public static bool operator <(P x, P y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.GreaterThan)] public static bool operator >(P x, P y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.LessThanOrEqual)] public static bool operator <=(P x, P y) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.GreaterThanOrEqual)] public static bool operator >=(P x, P y) => throw new NotImplementedException();
+
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] public bool Equals(P other) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] public int CompareTo(P other) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] public override bool Equals(object obj) => throw new NotImplementedException();
+        [ExpectedMethodSpecialName(MethodSpecialName.None)] public override int GetHashCode() => throw new NotImplementedException();
+      }
+    }
+
+    [TestCase(typeof(SpecialMethods.C))]
+    [TestCase(typeof(SpecialMethods.P))]
+    public void TestGetNameType(Type type)
+    {
+      foreach (var member in type.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)) {
+        if (member is MethodBase method) {
+          var attr = method.GetCustomAttribute<ExpectedMethodSpecialNameAttribute>();
+
+          if (attr == null)
+            continue;
+
+          var expected = method.GetCustomAttribute<ExpectedMethodSpecialNameAttribute>().Expected;
+
+          Assert.AreEqual(expected, method.GetNameType(), $"{type.FullName} : {method.Name}");
+        }
+      }
+    }
   }
 }
 
