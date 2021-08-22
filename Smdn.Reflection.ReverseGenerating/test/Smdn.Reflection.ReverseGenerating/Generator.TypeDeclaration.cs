98316637f2b74ab20206ebd54caf7147c0e34e48
smdn <smdn@smdn.jp>
2021-08-23 08:08:53 +0900

    fix test cases

--- Smdn.Reflection.ReverseGenerating/test/Smdn.Reflection.ReverseGenerating/Generator.TypeDeclaration.cs
+++ Smdn.Reflection.ReverseGenerating/test/Smdn.Reflection.ReverseGenerating/Generator.TypeDeclaration.cs
@@ -63,9 +63,15 @@ namespace Smdn.Reflection.ReverseGenerating {
         }
 
         namespace TranslateLanguagePrimitiveTypeDeclaration {
+#if NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
           [TypeDeclarationTestCase("public readonly struct int", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = true)]
           [TypeDeclarationTestCase("public readonly struct Int32", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = true)]
           [TypeDeclarationTestCase("public readonly struct Int32", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = false)]
+#elif NETFRAMEWORK && false
+          [TypeDeclarationTestCase("public struct int", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = true)]
+          [TypeDeclarationTestCase("public struct Int32", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = true)]
+          [TypeDeclarationTestCase("public struct Int32", typeof(int), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = false)]
+#endif
           [TypeDeclarationTestCase("public sealed class string", typeof(string), TranslateLanguagePrimitiveTypeDeclaration = true)]
           [TypeDeclarationTestCase("public sealed class String", typeof(string), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = true)]
           [TypeDeclarationTestCase("public sealed class String", typeof(string), TranslateLanguagePrimitiveTypeDeclaration = false, TypeWithNamespace = false)]
