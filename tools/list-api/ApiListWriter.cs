6acc6e8716799ea192c33a26abdf3f8957d24a78
smdn <smdn@smdn.jp>
2021-09-07 01:07:54 +0900

    split implementation out to ApiListWriter

---
+++ tools/list-api/ApiListWriter.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Text;
+
+using Smdn.Reflection;
+using Smdn.Reflection.ReverseGenerating;
+
+class ApiListWriter {
+  public TextWriter BaseWriter { get; }
+
+  private readonly Assembly assembly;
+  private readonly GeneratorOptions options;
+
+  public ApiListWriter(TextWriter baseWriter, Assembly assembly, GeneratorOptions options)
+  {
+    this.BaseWriter = baseWriter;
+    this.assembly = assembly;
+    this.options = options ?? new();
+  }
+
+  public void WriteAssemblyInfoHeader()
+  {
+    BaseWriter.WriteLine($"// {assembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product}");
+    BaseWriter.WriteLine($"//   Name: {assembly.GetName().Name}");
+    BaseWriter.WriteLine($"//   AssemblyVersion: {assembly.GetName().Version}");
+    BaseWriter.WriteLine($"//   InformationalVersion: {assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion}");
+    BaseWriter.WriteLine($"//   TargetFramework: {assembly.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName}");
+    BaseWriter.WriteLine($"//   Configuration: {assembly.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration}");
+    BaseWriter.WriteLine();
+  }
+
+  public void WriteExportedTypes()
+  {
+    var types = assembly.GetExportedTypes().Union(assembly.GetForwardedTypes());
+    var typeDeclarations = new StringBuilder(10240);
+    var referencingNamespaces = new HashSet<string>(StringComparer.Ordinal);
+
+    foreach (var ns in types.Select(t => t.Namespace).Distinct().OrderBy(n => n, StringComparer.Ordinal)) {
+      if (0 < typeDeclarations.Length)
+        typeDeclarations.AppendLine();
+
+      typeDeclarations.AppendLine($"namespace {ns} {{");
+
+      typeDeclarations.Append(GenerateTypeAndMemberDeclarations(1,
+                                                                types.Where(type => type.Namespace.Equals(ns, StringComparison.Ordinal))
+                                                                     .Where(type => !type.IsNested),
+                                                                referencingNamespaces,
+                                                                options));
+
+      typeDeclarations.AppendLine("}");
+    }
+
+    static int OrderOfRootNamespace(string ns)
+      => ns.Split('.')[0].StartsWith("System", StringComparison.Ordinal) ? 0 : 1;
+
+    foreach (var ns in referencingNamespaces.OrderBy(OrderOfRootNamespace).ThenBy(ns => ns, StringComparer.Ordinal)) {
+      BaseWriter.WriteLine($"using {ns};");
+    }
+
+    BaseWriter.WriteLine();
+    BaseWriter.WriteLine(typeDeclarations);
+  }
+
+  static string GenerateTypeAndMemberDeclarations(int nestLevel, IEnumerable<Type> types, ISet<string> referencingNamespaces, GeneratorOptions options)
+  {
+    var ret = new StringBuilder(10240);
+    var isPrevDelegate = false;
+
+    static int OrderOfType(Type t)
+    {
+      if (t.IsDelegate())   return 1;
+      if (t.IsInterface)    return 2;
+      if (t.IsEnum)         return 3;
+      if (t.IsClass)        return 4;
+      if (t.IsValueType)    return 5;
+
+      return int.MaxValue;
+    }
+
+    foreach (var type in types.OrderBy(OrderOfType).ThenBy(type => type.FullName, StringComparer.Ordinal)) {
+      var isDelegate = type.IsDelegate();
+
+      if (0 < ret.Length && !(isPrevDelegate && isDelegate))
+        ret.AppendLine();
+
+      ret.Append(GenerateTypeAndMemberDeclarations(nestLevel, type, referencingNamespaces, options));
+
+      isPrevDelegate = isDelegate;
+    }
+
+    return ret.ToString();
+  }
+
+  // TODO: unsafe types
+  static string GenerateTypeAndMemberDeclarations(int nestLevel, Type t, ISet<string> referencingNamespaces, GeneratorOptions options)
+  {
+    if (options == null)
+      throw new ArgumentNullException(nameof(options));
+
+    var ret = new StringBuilder(1024);
+    var indent = string.Concat(Enumerable.Repeat(options.Indent, nestLevel));
+
+    if (t.GetCustomAttribute<TypeForwardedFromAttribute>() is not null) {
+      ret.Append(indent)
+         .AppendLine($"// Forwarded to \"{t.Assembly.GetName().FullName}\"");
+    }
+
+    // TODO: AttributeTargets.GenericParameter, AttributeTargets.ReturnValue, AttributeTargets.Parameter
+    foreach (var attr in Generator.GenerateAttributeList(t, null, options)) {
+      ret.Append(indent)
+         .AppendLine(attr);
+    }
+
+    var typeDeclarationLines = Generator.GenerateTypeDeclarationWithExplicitBaseTypeAndInterfaces(t, referencingNamespaces, options).ToList();
+
+    for (var index = 0; index < typeDeclarationLines.Count; index++) {
+      if (0 < index)
+        ret.AppendLine();
+
+      ret.Append(indent).Append(typeDeclarationLines[index]);
+    }
+
+    var hasContent = !t.IsDelegate();
+
+    if (hasContent) {
+      if (1 < typeDeclarationLines.Count) // multiline declaration
+        ret.AppendLine().Append(indent).AppendLine("{");
+      else
+        ret.AppendLine(" {");
+
+      ret.Append(GenerateTypeContentDeclarations(nestLevel + 1, t, referencingNamespaces, options));
+
+      ret.Append(indent).AppendLine("}");
+    }
+    else {
+      ret.AppendLine();
+    }
+
+    return ret.ToString();
+  }
+
+  static string GenerateTypeContentDeclarations(int nestLevel, Type t, ISet<string> referencingNamespaces, GeneratorOptions options)
+  {
+    if (options == null)
+      throw new ArgumentNullException(nameof(options));
+
+    const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly;
+
+    var members = t.GetMembers(bindingFlags).OrderBy(f => f.Name, StringComparer.Ordinal).ToList();
+    var exceptingMembers = new List<MemberInfo>();
+    var nestedTypes = new List<Type>();
+
+    foreach (var member in members) {
+      if (member is PropertyInfo p) {
+        // remove get/set accessor of properties
+        exceptingMembers.AddRange(p.GetAccessors(true));
+      }
+      else if (member is EventInfo e) {
+        // remove add/remove/raise method of events
+        exceptingMembers.AddRange(e.GetMethods(true));
+      }
+      else if (member.MemberType == MemberTypes.NestedType) {
+        exceptingMembers.Add(member);
+        nestedTypes.Add(member as Type);
+      }
+    }
+
+    var ret = new StringBuilder(1024);
+    var prevMemberType = (MemberTypes?)null;
+
+    if (0 < nestedTypes.Count) {
+      ret.Append(GenerateTypeAndMemberDeclarations(nestLevel,
+                                                   nestedTypes.Where(nestedType => !(options.IgnorePrivateOrAssembly && (nestedType.IsNestedPrivate || nestedType.IsNestedAssembly))),
+                                                   referencingNamespaces,
+                                                   options));
+
+      if (0 < ret.Length)
+        prevMemberType = MemberTypes.NestedType;
+    }
+
+    var indent = string.Concat(Enumerable.Repeat(options.Indent, nestLevel));
+    var memberAndDeclarations = new List<(MemberInfo member, string declaration)>();
+
+    foreach (var member in members.Except(exceptingMembers).OrderBy(OrderOfMember).ThenBy(m => m.Name, StringComparer.Ordinal)) {
+      try {
+        var declaration = Generator.GenerateMemberDeclaration(member, referencingNamespaces, options);
+
+        if (declaration == null)
+          continue; // is private or assembly
+
+        memberAndDeclarations.Add((member, declaration));
+      }
+      catch (Exception ex) {
+        Console.Error.WriteLine($"reflection error at member {t.FullName}.{member.Name}");
+        Console.Error.WriteLine(ex);
+      }
+    }
+
+    foreach (var (member, declaration) in memberAndDeclarations.OrderBy(p => OrderOfMember(p.member)).ThenBy(p => p.member.Name, StringComparer.Ordinal).ThenBy(p => p.declaration, StringComparer.Ordinal)) {
+      if (prevMemberType != null && prevMemberType != member.MemberType)
+        ret.AppendLine();
+
+      // TODO: AttributeTargets.ReturnValue, AttributeTargets.Parameter
+      foreach (var attr in Generator.GenerateAttributeList(member, null, options)) {
+        ret.Append(indent).AppendLine(attr);
+      }
+
+      ret.Append(indent).AppendLine(declaration);
+
+      prevMemberType = member.MemberType;
+    }
+
+    return ret.ToString();
+
+    static int OrderOfMember(MemberInfo member) => member switch {
+      EventInfo e           => 1,
+      FieldInfo f           => 2,
+      ConstructorInfo ctor  => 3,
+      PropertyInfo p        => 4,
+      MethodInfo m          => 5,
+      _                     => int.MaxValue,
+    };
+  }
+}
--- tools/list-api/Main.cs
+++ tools/list-api/Main.cs
@@ -1,30 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 using System.Text;
+using System.Reflection;
+using System.Runtime.Versioning;
 
-using Smdn.Reflection;
 using Smdn.Reflection.ReverseGenerating;
-
-class Options : GeneratorOptions {
-  public Options Clone()
-  {
-    return (Options)MemberwiseClone();
-  }
-
-  public static Options Default { get; } = new Options();
-}
 
 class ListApi {
   static void Main(string[] args)
   {
     var libs = new List<string>();
-    var options = new Options();
+    var options = new GeneratorOptions();
     var showUsage = false;
     var stdout = false;
 
@@ -91,225 +78,16 @@ class ListApi {
       var defaultOutputFileName = $"{assemblyIdentifier}.apilist.cs";
 
       using (Stream outputStream = stdout ? Console.OpenStandardOutput() : File.Open(defaultOutputFileName, FileMode.Create)) {
-        using (var writer = new StreamWriter(outputStream, stdout ? Console.OutputEncoding : new UTF8Encoding(false))) {
-          DisplayAssemblyInfo(writer, assm);
+        using (var outputWriter = new StreamWriter(outputStream, stdout ? Console.OutputEncoding : new UTF8Encoding(false))) {
+          var writer = new ApiListWriter(outputWriter, assm, options);
 
-          DisplayExportedTypes(writer, assm, options);
+          writer.WriteAssemblyInfoHeader();
+
+          writer.WriteExportedTypes();
         }
       }
 
       Console.Error.WriteLine("done");
-    }
-  }
-
-  static void DisplayAssemblyInfo(TextWriter writer, Assembly assm)
-  {
-    writer.WriteLine($"// {assm.GetCustomAttribute<AssemblyProductAttribute>()?.Product}");
-    writer.WriteLine($"//   Name: {assm.GetName().Name}");
-    writer.WriteLine($"//   TargetFramework: {assm.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName}");
-    writer.WriteLine($"//   AssemblyVersion: {assm.GetName().Version}");
-    writer.WriteLine($"//   InformationalVersion: {assm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion}");
-    writer.WriteLine();
-  }
-
-  static void DisplayExportedTypes(TextWriter writer, Assembly assm, Options options)
-  {
-    if (options == null)
-      throw new ArgumentNullException(nameof(options));
-
-    var types = assm.GetExportedTypes().Union(assm.GetForwardedTypes());
-    var typeDeclarations = new StringBuilder(10240);
-    var referencingNamespaces = new HashSet<string>(StringComparer.Ordinal);
-
-    foreach (var ns in types.Select(t => t.Namespace).Distinct().OrderBy(n => n, StringComparer.Ordinal)) {
-      if (0 < typeDeclarations.Length)
-        typeDeclarations.AppendLine();
-
-      typeDeclarations.AppendLine($"namespace {ns} {{");
-
-      typeDeclarations.Append(GenerateTypeAndMemberDeclarations(1,
-                                                                types.Where(type => type.Namespace.Equals(ns, StringComparison.Ordinal))
-                                                                     .Where(type => !type.IsNested),
-                                                                referencingNamespaces,
-                                                                options));
-
-      typeDeclarations.AppendLine("}");
-    }
-
-    foreach (var ns in referencingNamespaces.OrderBy(OrderOfRootNamespace).ThenBy(ns => ns, StringComparer.Ordinal)) {
-      writer.WriteLine($"using {ns};");
-    }
-
-    writer.WriteLine();
-    writer.WriteLine(typeDeclarations);
-
-    int OrderOfRootNamespace(string ns)
-    {
-      return ns.Split('.')[0].StartsWith("System", StringComparison.Ordinal) ? 0 : 1;
-    }
-  }
-
-  static string GenerateTypeAndMemberDeclarations(int nestLevel, IEnumerable<Type> types, ISet<string> referencingNamespaces, Options options)
-  {
-    var ret = new StringBuilder(10240);
-    var isPrevDelegate = false;
-
-    foreach (var type in types.OrderBy(OrderOfType).ThenBy(type => type.FullName, StringComparer.Ordinal)) {
-      var isDelegate = type.IsDelegate();
-
-      if (0 < ret.Length && !(isPrevDelegate && isDelegate))
-        ret.AppendLine();
-
-      ret.Append(GenerateTypeAndMemberDeclarations(nestLevel, type, referencingNamespaces, options));
-
-      isPrevDelegate = isDelegate;
-    }
-
-    return ret.ToString();
-
-    int OrderOfType(Type t)
-    {
-      if (t.IsDelegate())   return 1;
-      if (t.IsInterface)    return 2;
-      if (t.IsEnum)         return 3;
-      if (t.IsClass)        return 4;
-      if (t.IsValueType)    return 5;
-
-      return int.MaxValue;
-    }
-  }
-
-  // TODO: unsafe types
-  internal static string GenerateTypeAndMemberDeclarations(int nestLevel, Type t, ISet<string> referencingNamespaces, Options options)
-  {
-    if (options == null)
-      throw new ArgumentNullException(nameof(options));
-
-    var ret = new StringBuilder(1024);
-    var indent = string.Concat(Enumerable.Repeat(options.Indent, nestLevel));
-
-    if (t.GetCustomAttribute<TypeForwardedFromAttribute>() is not null) {
-      ret.Append(indent)
-         .AppendLine($"// Forwarded to \"{t.Assembly.GetName().FullName}\"");
-    }
-
-    // TODO: AttributeTargets.GenericParameter, AttributeTargets.ReturnValue, AttributeTargets.Parameter
-    foreach (var attr in Generator.GenerateAttributeList(t, null, options)) {
-      ret.Append(indent)
-         .AppendLine(attr);
-    }
-
-    var typeDeclarationLines = Generator.GenerateTypeDeclarationWithExplicitBaseTypeAndInterfaces(t, referencingNamespaces, options).ToList();
-
-    for (var index = 0; index < typeDeclarationLines.Count; index++) {
-      if (0 < index)
-        ret.AppendLine();
-
-      ret.Append(indent).Append(typeDeclarationLines[index]);
-    }
-
-    var hasContent = !t.IsDelegate();
-
-    if (hasContent) {
-      if (1 < typeDeclarationLines.Count) // multiline declaration
-        ret.AppendLine().Append(indent).AppendLine("{");
-      else
-        ret.AppendLine(" {");
-
-      ret.Append(GenerateTypeContentDeclarations(nestLevel + 1, t, referencingNamespaces, options));
-
-      ret.Append(indent).AppendLine("}");
-    }
-    else {
-      ret.AppendLine();
-    }
-
-    return ret.ToString();
-  }
-
-  internal static string GenerateTypeContentDeclarations(int nestLevel, Type t, ISet<string> referencingNamespaces, Options options)
-  {
-    if (options == null)
-      throw new ArgumentNullException(nameof(options));
-
-    const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly;
-
-    var members = t.GetMembers(bindingFlags).OrderBy(f => f.Name, StringComparer.Ordinal).ToList();
-    var exceptingMembers = new List<MemberInfo>();
-    var nestedTypes = new List<Type>();
-
-    foreach (var member in members) {
-      if (member is PropertyInfo p) {
-        // remove get/set accessor of properties
-        exceptingMembers.AddRange(p.GetAccessors(true));
-      }
-      else if (member is EventInfo e) {
-        // remove add/remove/raise method of events
-        exceptingMembers.AddRange(e.GetMethods(true));
-      }
-      else if (member.MemberType == MemberTypes.NestedType) {
-        exceptingMembers.Add(member);
-        nestedTypes.Add(member as Type);
-      }
-    }
-
-    var ret = new StringBuilder(1024);
-    var prevMemberType = (MemberTypes?)null;
-
-    if (0 < nestedTypes.Count) {
-      ret.Append(GenerateTypeAndMemberDeclarations(nestLevel,
-                                                   nestedTypes.Where(nestedType => !(options.IgnorePrivateOrAssembly && (nestedType.IsNestedPrivate || nestedType.IsNestedAssembly))),
-                                                   referencingNamespaces,
-                                                   options));
-
-      if (0 < ret.Length)
-        prevMemberType = MemberTypes.NestedType;
-    }
-
-    var indent = string.Concat(Enumerable.Repeat(options.Indent, nestLevel));
-    var memberAndDeclarations = new List<(MemberInfo member, string declaration)>();
-
-    foreach (var member in members.Except(exceptingMembers).OrderBy(OrderOfMember).ThenBy(m => m.Name, StringComparer.Ordinal)) {
-      try {
-        var declaration = Generator.GenerateMemberDeclaration(member, referencingNamespaces, options);
-
-        if (declaration == null)
-          continue; // is private or assembly
-
-        memberAndDeclarations.Add((member, declaration));
-      }
-      catch (Exception ex) {
-        Console.Error.WriteLine($"reflection error at member {t.FullName}.{member.Name}");
-        Console.Error.WriteLine(ex);
-      }
-    }
-
-    foreach (var (member, declaration) in memberAndDeclarations.OrderBy(p => OrderOfMember(p.member)).ThenBy(p => p.member.Name, StringComparer.Ordinal).ThenBy(p => p.declaration, StringComparer.Ordinal)) {
-      if (prevMemberType != null && prevMemberType != member.MemberType)
-        ret.AppendLine();
-
-      // TODO: AttributeTargets.ReturnValue, AttributeTargets.Parameter
-      foreach (var attr in Generator.GenerateAttributeList(member, null, options)) {
-        ret.Append(indent).AppendLine(attr);
-      }
-
-      ret.Append(indent).AppendLine(declaration);
-
-      prevMemberType = member.MemberType;
-    }
-
-    return ret.ToString();
-
-    int OrderOfMember(MemberInfo member)
-    {
-      switch (member) {
-        case EventInfo e:           return 1;
-        case FieldInfo f:           return 2;
-        case ConstructorInfo ctor:  return 3;
-        case PropertyInfo p:        return 4;
-        case MethodInfo m:          return 5;
-        default:                    return int.MaxValue;
-      }
     }
   }
 }
