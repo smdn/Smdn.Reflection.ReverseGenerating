befd55f21ae461d781a725d1bd71cd866051a990
smdn <smdn@smdn.jp>
2021-09-12 23:54:44 +0900

    add FrameworkMonikers

---
+++ tools/list-api/src/FrameworkMonikers.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Runtime.Versioning;
+
+namespace Smdn.Reflection.ReverseGenerating.ListApi;
+
+public static class FrameworkMonikers {
+  private static readonly Version versionNET5OrGreater = new Version(5, 0);
+
+  /// <remarks>
+  ///   <see cref="FrameworkName.Profile"/> of <paramref name="frameworkName"/> is not supported currently.</remarks>
+  ///   <paramref name="osSpecifier"/> is not supported currently.
+  /// </remarks>
+  public static bool TryGetMoniker(FrameworkName frameworkName, string osSpecifier, out string frameworkMoniker)
+  {
+    if (frameworkName is null)
+      throw new ArgumentNullException(nameof(frameworkName));
+
+    frameworkMoniker = default;
+
+    // TODO: frameworkName.Profile, osSpecifier
+    switch (frameworkName.Identifier) {
+      case ".NETCoreApp" when versionNET5OrGreater <= frameworkName.Version:
+        frameworkMoniker = $"net{frameworkName.Version.Major}.{frameworkName.Version.Minor}";
+        return true;
+
+      case ".NETCoreApp":
+        frameworkMoniker = $"netcoreapp{frameworkName.Version.Major}.{frameworkName.Version.Minor}";
+        return true;
+
+      case ".NETStandard":
+        frameworkMoniker = $"netstandard{frameworkName.Version.Major}.{frameworkName.Version.Minor}";
+        return true;
+
+      case ".NETFramework" when frameworkName.Version.Build == -1:
+        frameworkMoniker = $"net{frameworkName.Version.Major}{frameworkName.Version.Minor}";
+        return true;
+
+      case ".NETFramework":
+        frameworkMoniker = $"net{frameworkName.Version.Major}{frameworkName.Version.Minor}{frameworkName.Version.Build}";
+        return true;
+
+      default:
+        return false;
+    }
+  }
+}
---
+++ tools/list-api/test/FrameworkMonikers.cs
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.Runtime.Versioning;
+using NUnit.Framework;
+
+namespace Smdn.Reflection.ReverseGenerating.ListApi;
+
+[TestFixture]
+class FrameworkMonikersTests {
+  [TestCase(".NETCoreApp,Version=v6.0", "net6.0")]
+  [TestCase(".NETCoreApp,Version=v5.0", "net5.0")]
+  [TestCase(".NETCoreApp,Version=v3.1", "netcoreapp3.1")]
+  [TestCase(".NETStandard,Version=v2.1", "netstandard2.1")]
+  [TestCase(".NETStandard,Version=v1.6", "netstandard1.6")]
+  [TestCase(".NETFramework,Version=v4.7.1", "net471")]
+  [TestCase(".NETFramework,Version=v4.5", "net45")]
+  [TestCase(".NETFramework,Version=v4.0", "net40")]
+  public void TryGetMoniker(string input, string expected)
+  {
+    Assert.IsTrue(FrameworkMonikers.TryGetMoniker(new FrameworkName(input), osSpecifier: null, out var moniker));
+    Assert.AreEqual(expected, moniker, nameof(moniker));
+  }
+
+  [Test]
+  public void TryGetMoniker_ArgumentNull()
+  {
+    FrameworkName name = null;
+
+    Assert.Throws<ArgumentNullException>(() => FrameworkMonikers.TryGetMoniker(name, osSpecifier: null, out _));
+  }
+}
