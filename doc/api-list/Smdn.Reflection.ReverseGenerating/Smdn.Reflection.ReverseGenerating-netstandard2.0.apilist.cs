238c2935cebe8eb4a12ba2a21a5f693ad7410118
smdn <smdn@smdn.jp>
2021-12-30 22:20:38 +0900

    update API list Smdn.Reflection.ReverseGenerating-1.0.1

--- doc/api-list/Smdn.Reflection.ReverseGenerating/Smdn.Reflection.ReverseGenerating-netstandard2.0.apilist.cs
+++ doc/api-list/Smdn.Reflection.ReverseGenerating/Smdn.Reflection.ReverseGenerating-netstandard2.0.apilist.cs
@@ -1,7 +1,7 @@
-// Smdn.Reflection.ReverseGenerating.dll (Smdn.Reflection.ReverseGenerating-1.0 (netstandard2.0))
+// Smdn.Reflection.ReverseGenerating.dll (Smdn.Reflection.ReverseGenerating-1.0.1 (netstandard2.0))
 //   Name: Smdn.Reflection.ReverseGenerating
-//   AssemblyVersion: 1.0.0.0
-//   InformationalVersion: 1.0 (netstandard2.0)
+//   AssemblyVersion: 1.0.1.0
+//   InformationalVersion: 1.0.1 (netstandard2.0)
 //   TargetFramework: .NETStandard,Version=v2.0
 //   Configuration: Release
 
