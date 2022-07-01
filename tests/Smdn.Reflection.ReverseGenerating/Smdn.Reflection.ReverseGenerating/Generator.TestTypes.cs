bfae424e3e11e008b4ab20095ca6fdf8decf5603
smdn <smdn@smdn.jp>
2022-07-01 20:13:09 +0900

    validate availablity of System.Runtime.CompilerServices.ModuleInitializerAttribute

--- tests/Smdn.Reflection.ReverseGenerating/Smdn.Reflection.ReverseGenerating/Generator.TestTypes.cs
+++ tests/Smdn.Reflection.ReverseGenerating/Smdn.Reflection.ReverseGenerating/Generator.TestTypes.cs
@@ -1,19 +1,27 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NET5_0_OR_GREATER
+#define SYSTEM_RUNTIME_COMPILERSERVICES_MODULEINITIALIZERATTRIBUTE
+#endif
+
 using System;
+#if SYSTEM_RUNTIME_COMPILERSERVICES_MODULEINITIALIZERATTRIBUTE
 using System.Runtime.CompilerServices;
+#endif
 
 namespace Smdn.Reflection.ReverseGenerating.TestTypes;
 
 public static class NS {
   public const string Namespace = "Smdn.Reflection.ReverseGenerating.TestTypes";
 
+#if SYSTEM_RUNTIME_COMPILERSERVICES_MODULEINITIALIZERATTRIBUTE
   [ModuleInitializer]
   public static void ValidateConstant()
   {
     if (!string.Equals(Namespace, typeof(NS).Namespace, StringComparison.Ordinal))
       throw new InvalidOperationException("constant value of namespace does not match with its actual declaration");
   }
+#endif
 }
 
 public class C {
