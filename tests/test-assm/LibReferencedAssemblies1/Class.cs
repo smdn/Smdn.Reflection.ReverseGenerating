29a53974b82a7b53a37558c007c4512a8465af53
smdn <smdn@smdn.jp>
2023-01-11 22:59:09 +0900

    add tests for ApiListWriter.WriteReferencedAssemblies

---
+++ tests/test-assm/LibReferencedAssemblies1/Class.cs
@@ -0,0 +1,6 @@
+public static class C {
+  public static void M(int p) { }
+  public static void M(System.Uri p) { }
+  public static void M(System.Threading.Mutex p) { }
+  public static void M(System.Xml.XmlDocument p) { }
+}
